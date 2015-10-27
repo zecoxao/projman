38081cd5256c788318a3daf434e99869b33ef138
zecoxao <zecoxao@hotmail.com>
2015-10-27 14:56:09 +0000

    final

--- projman/index.php
+++ projman/index.php
@@ -1,5 +1,33 @@
 <?php
 
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "root";
+$dbname = "root";
+
+$link = mysqli_connect($dbhost, $dbuser, $dbpass);
+if( mysqli_query($link, "USE $dbname") == FALSE){
+	mysqli_close($link);
+	
+
+	$link = mysqli_connect($dbhost, $dbuser, $dbpass);
+	mysqli_query($link, "CREATE DATABASE $dbname");
+	mysqli_close($link);
+	$file =dirname(__FILE__).'\\'.'bd.sql';
+	$mySQLDir='"C:\\xampp\\mysql\\bin\\mysql.exe"';
+
+
+
+	if ($dbpass != '') {
+		$cmd = $mySQLDir.' -h '.$dbhost.' --user='.$dbuser.' --password='.$dbpass.' --database='.$dbname.' < "'.$file.'"';
+
+	} else {
+		$cmd = $mySQLDir.' -h '.$dbhost.' --user='.$dbuser.' < "'.$file.'"';
+	}
+
+	exec($cmd,$out,$retval);
+}
+
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
